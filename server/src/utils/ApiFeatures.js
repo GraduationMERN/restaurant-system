266637d8f3705f49fb93e057439b4505221d7407
mohammed Khaled <72397920+mohammed-khaled-5@users.noreply.github.com>
2025-11-11 20:43:54 +0200

    test

---
+++ server/src/utils/ApiFeatures.js
@@ -0,0 +1,65 @@
+export class ApiFeatures {
+  constructor(mongooseQuery, queryString) {
+    this.mongooseQuery = mongooseQuery;
+    this.queryString = queryString;
+  }
+
+  //1-pagination
+  paginate() {
+    let limit = this.queryString.limit * 1 || 10; 
+    let page = this.queryString.page * 1 || 1;
+    if (this.queryString.page <= 0) page = 1;
+    let skip = (page - 1) * limit;
+    this.page = page;
+    this.mongooseQuery.skip(skip).limit(limit);
+    return this;
+  }
+
+  //2-filter
+  filter() {
+    let filterObj = { ...this.queryString };
+    let exclude = ["page", "sort", "fields", "keyword"];
+    exclude.forEach((e) => {
+      delete filterObj[e];
+    });
+    filterObj = JSON.stringify(filterObj);
+    filterObj = filterObj.replace(
+      /\b(gt|gte|lt|lte)\b/g,
+      (match) => `$${match}`
+    );
+    filterObj = JSON.parse(filterObj);
+    this.mongooseQuery.find(filterObj);
+    return this;
+  }
+
+  //3-sort
+  sort() {
+    if (this.queryString.sort) {
+      let sortedBy = this.queryString.sort.split(",").join(" ");
+      this.mongooseQuery.sort(sortedBy);
+    }
+    return this;
+  }
+
+  //4-search
+  search() {
+    if (this.queryString.keyword) {
+      this.mongooseQuery.find({
+        $or: [
+          { title: { $regex: this.queryString.keyword, $options: "i" } },
+          { description: { $regex: this.queryString.keyword, $options: "i" } },
+        ],
+      });
+    }
+    return this;
+  }
+
+  //5-select fields
+  fields() {
+    if (this.queryString.fields) {
+      let fields = this.queryString.fields.split(",").join(" ");
+      this.mongooseQuery.select(fields);
+    }
+    return this;
+  }
+}
