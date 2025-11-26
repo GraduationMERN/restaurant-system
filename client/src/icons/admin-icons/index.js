f1e9c867fd50b229fce991fb875cd3be6a2c03ce
Mostafa Youns <mostafayouns27@gmail.com>
2025-11-26 13:29:12 +0200

    adding icons of admin page

---
+++ client/src/icons/admin-icons/index.js
@@ -0,0 +1,43 @@
+import { ReactComponent as PlusIcon } from "./plus.svg?react";
+import { ReactComponent as CheckCircleIcon } from "./check-circle.svg?react";
+import { ReactComponent as ArrowUpIcon } from "./arrow-up.svg?react";
+import { ReactComponent as ArrowDownIcon } from "./arrow-down.svg?react";
+import { ReactComponent as GridIcon } from "./grid.svg?react";
+import { ReactComponent as GroupIcon } from "./group.svg?react";
+import { ReactComponent as BoxIconLine } from "./box-line.svg?react";
+import { ReactComponent as ShootingStarIcon } from "./shooting-star.svg?react";
+import { ReactComponent as DollarLineIcon } from "./dollar-line.svg?react";
+import { ReactComponent as TrashBinIcon } from "./trash.svg?react";
+import { ReactComponent as PencilIcon } from "./pencil.svg?react";
+import { ReactComponent as ChevronDownIcon } from "./chevron-down.svg?react";
+import { ReactComponent as CalenderIcon } from "./calender-line.svg?react";
+import { ReactComponent as UserCircleIcon } from "./user-circle.svg?react";
+import { ReactComponent as TaskIcon } from "./task-icon.svg?react";
+import { ReactComponent as BoxCubeIcon } from "./box-cube.svg?react";
+import { ReactComponent as PlugInIcon } from "./plug-in.svg?react";
+import { ReactComponent as HorizontaLDots } from "./horizontal-dots.svg?react";
+import { ReactComponent as MoreDotIcon } from "./moredot.svg?react";
+import { ReactComponent as SettingsGearIcon } from "./settings-gear.svg?react";
+
+export {
+  MoreDotIcon,
+  GridIcon,
+  PlusIcon,
+  CheckCircleIcon,
+  ArrowUpIcon,
+  ArrowDownIcon,
+  GroupIcon,
+  BoxIconLine,
+  ShootingStarIcon,
+  DollarLineIcon,
+  TrashBinIcon,
+  PencilIcon,
+  ChevronDownIcon,
+  CalenderIcon,
+  UserCircleIcon,
+  TaskIcon,
+  BoxCubeIcon,
+  PlugInIcon,
+  HorizontaLDots,
+  SettingsGearIcon,
+};
