8c1ee0a83d80af1c0d8421a1d27d72ddec72f5a7
Nour <kkid607@gmail.com>
2025-11-30 18:30:28 +0200

    registration + login + logout

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,7 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import rewardReducer from "./slices/rewardSlice";
+import authReducer from "./slices/authSlice";
+
 export const store = configureStore({
   reducer: {
     reward: rewardReducer,
+    auth: authReducer,
   },
 });
