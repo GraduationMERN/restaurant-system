8f917757a5f0b47c18d5cc932aa189b4b90797f7
neggmmm <kekkdead@gmail.com>
2025-12-14 12:24:20 +0200

    fix adding logo

--- server/app.js
+++ server/app.js
@@ -76,7 +76,8 @@ app.post(
 );
 
 // Now apply regular JSON parsing for other routes
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.use(cookieParser());
 app.use(requestIdMiddleware);
 // app.use(requestLogger);
